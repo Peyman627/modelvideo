1fbef1bad380c05a9834148161bc59d994d3988d
Peyman Hassani <hassani.peyman627@gmail.com>
2020-11-12 17:22:05 +0330

    models created for example app

--- example/models.py
+++ example/models.py
@@ -1,6 +1,24 @@
 from django.db import models
-from django.db.models.fields import CharField
 
 
 class Company(models.Model):
-    name = CharField(max_length=20)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Programmer(models.Model):
+    name = models.CharField(max_length=20)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    languages = models.ManyToManyField(Language)
+
+    def __str__(self):
+        return self.name
